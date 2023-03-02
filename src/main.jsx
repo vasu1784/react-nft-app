f98a194469dc6ce6561cbda89242ba0aa73701d9
Vasu Chaudhary <vasu.chaudhary@grazitti.local>
2023-03-02 17:10:31 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,34 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Signup from './pages/Signup';
-import Layout from './components/Layout';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Layout from "./components/Layout";
 
-
-
-const router =createBrowserRouter ([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <App />,
+        },
+        {
+          path: "/signup",
+          element: <Signup />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element : <Layout/>,
-    children:[
-      {
-        path : "/",
-        element : <App/>
-      },
-      {
-        path : "/signup",
-        element : <Signup/>
-      }
-    ]
+    basename: "/react-nft-app/",
   }
+);
 
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
