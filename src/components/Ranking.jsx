3bd6fab2d9f3063c1bb79fc1e727a1a9c3d1e4ed
Vasu Chaudhary <vasu.chaudhary@grazitti.local>
2023-03-30 15:42:18 +0530

    redux

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -2,6 +2,12 @@ import rankingbg from "../assets/rankingbg.svg"
 import avtar from "../assets/rankingavtar.svg"
 
 const Ranking=()=>{
+
+   const working = (()=>{
+      alert("value incremented")
+   })
+    
+
     return(
 
               <div className="container-fluid mt-3 p-lg-0">
@@ -17,9 +23,33 @@ const Ranking=()=>{
                         </div>
                                          
                        <h3 className="rankheading">AnikaKid</h3>       
+                                       
+                             <div className="row align-items-center innercounter">
+                                 <div className="col-lg-8">
+                                          <h4>Increment/Decrement Nft Values Here By One Click</h4>
+                                          <h4>Press to increase/decrement value</h4>
+                                          <div className="gap-3 d-flex">
+                                          <button type="button" class="btn btn-primary" onClick={working}>+</button>
+                                        <button type="button" class="btn btn-primary">-</button>
+                                          </div>
+                                      
+                                 </div>
+                                  
+                                   <div className="quantity col-lg-4">
+                                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nft value"/>
+                                   </div>
+                                
+
+                                </div>      
+                            
+                                  
 
                        </div>
                             
+                                   
+                
+
+
               </div>
     )
 }
